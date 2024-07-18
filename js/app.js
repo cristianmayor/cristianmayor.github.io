7c6fed21f0f29674e76ec06450173a5a6ebcab33
cristianmayor <cristian.mayor0723@gmail.com>
2024-07-18 14:46:02 -0500

    Cambios jueves 18

--- js/app.js
+++ js/app.js
@@ -99,3 +99,42 @@ var mySwiper = new Swiper(".swiper-container", {
     nextEl: ".swiper-button-next",
   },
 });
+
+
+document.getElementById('openPopup').addEventListener('click', function() {
+  document.getElementById('popup').style.display = 'flex';
+});
+
+document.getElementById('openPopup1').addEventListener('click', function() {
+  document.getElementById('popup1').style.display = 'flex';
+});
+
+document.getElementById('openPopup2').addEventListener('click', function() {
+  document.getElementById('popup2').style.display = 'flex';
+});
+
+document.querySelectorAll('.close').forEach(function(closeButton) {
+  closeButton.addEventListener('click', function() {
+    const popupId = this.getAttribute('data-close');
+    document.getElementById(popupId).style.display = 'none';
+  });
+});
+
+document.getElementById('closePopup').addEventListener('click', function() {
+  document.getElementById('popup').style.display = 'none';
+});
+
+document.getElementById('closePopup1').addEventListener('click', function() {
+  document.getElementById('popup1').style.display = 'none';
+});
+
+document.getElementById('closePopup2').addEventListener('click', function() {
+  document.getElementById('popup2').style.display = 'none';
+});
+
+// Cerrar el popup al hacer clic fuera de su contenido
+window.addEventListener('click', function(event) {
+  if (event.target.classList.contains('popup')) {
+      event.target.style.display = 'none';
+  }
+});
